2694afb4a5c51961647885e2db0fa48db7e2eb08
JHOSEPH <jhoseph@ZEROCOOL.ZEROCOOL>
2025-05-27 23:40:29 -0400

    fix agregue la redireccion del logo principal a /

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,26 @@
 import { BookOpenIcon, UserIcon } from "lucide-react";
+import { Link } from "react-router-dom";
 export const Header = () => {
   return (
     <header className="bg-gradient-to-r from-indigo-600 to-blue-500 text-white shadow-md">
       <div className="container mx-auto px-4 py-3 flex justify-between items-center">
         <div className="flex items-center space-x-2">
           <BookOpenIcon size={24} />
-          <span className="text-xl font-semibold">EduShapes</span>
+          <Link to='/'  className="text-xl font-semibold">EduShapes</Link>
         </div>
         <nav className="hidden md:flex items-center space-x-6">
-          <a href="#" className="hover:text-indigo-100 transition-colors">
+          <Link to="/" className="hover:text-indigo-100 transition-colors">
             Inicio
-          </a>
-          <a href="#" className="hover:text-indigo-100 transition-colors">
+          </Link>
+          <Link to="#" className="hover:text-indigo-100 transition-colors">
             Ejercicios
-          </a>
-          <a href="#" className="hover:text-indigo-100 transition-colors">
+          </Link>
+          <Link to="#" className="hover:text-indigo-100 transition-colors">
             Recursos
-          </a>
-          <a href="#" className="hover:text-indigo-100 transition-colors">
+          </Link>
+          <Link to="#" className="hover:text-indigo-100 transition-colors">
             Ayuda
-          </a>
+          </Link>
         </nav>
         <button className="bg-white text-indigo-600 px-4 py-2 rounded-lg flex items-center space-x-2 hover:bg-indigo-50 transition-colors">
           <UserIcon size={18} />
